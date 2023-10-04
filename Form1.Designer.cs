3fa2cf9d4199825ba10aca1b0ad7cce33b8888c7
sirapope <sirapope@profile.co.th>
2023-10-04 16:04:08 +0700

    test DB Connection

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,45 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
+            btnLogin = new Button();
             checkBox1 = new CheckBox();
+            dgvFound = new DataGridView();
+            panel1 = new Panel();
+            txtUser = new TextBox();
+            panel2 = new Panel();
+            ((System.ComponentModel.ISupportInitialize)dgvFound).BeginInit();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
             SuspendLayout();
             // 
-            // button1
+            // btnLogin
             // 
-            button1.Location = new Point(151, 122);
-            button1.Name = "button1";
-            button1.Size = new Size(164, 57);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            btnLogin.Location = new Point(26, 381);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(164, 57);
+            btnLogin.TabIndex = 0;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += btnLogin_Click;
             // 
             // checkBox1
             // 
             checkBox1.AutoSize = true;
-            checkBox1.Location = new Point(167, 229);
+            checkBox1.Location = new Point(217, 401);
             checkBox1.Name = "checkBox1";
             checkBox1.Size = new Size(83, 19);
             checkBox1.TabIndex = 1;
             checkBox1.Text = "checkBox1";
             checkBox1.UseVisualStyleBackColor = true;
             // 
+            // dgvFound
+            // 
+            dgvFound.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvFound.Dock = DockStyle.Fill;
+            dgvFound.Location = new Point(0, 0);
+            dgvFound.Name = "dgvFound";
+            dgvFound.RowTemplate.Height = 25;
+            dgvFound.Size = new Size(417, 450);
+            dgvFound.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(txtUser);
+            panel1.Controls.Add(btnLogin);
+            panel1.Controls.Add(checkBox1);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(383, 450);
+            panel1.TabIndex = 3;
+            // 
+            // txtUser
+            // 
+            txtUser.Location = new Point(26, 330);
+            txtUser.Name = "txtUser";
+            txtUser.Size = new Size(164, 23);
+            txtUser.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(dgvFound);
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(383, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(417, 450);
+            panel2.TabIndex = 4;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(checkBox1);
-            Controls.Add(button1);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
             Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dgvFound).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
-        private Button button1;
+        private Button btnLogin;
         private CheckBox checkBox1;
+        private DataGridView dgvFound;
+        private Panel panel1;
+        private TextBox txtUser;
+        private Panel panel2;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,43 @@
+using System.Data;
+using System.Data.SqlClient;
+
 namespace CsharpWithGitIgnore
 {
     public partial class Form1 : Form
     {
+        public int showData(string sql, string conn, DataGridView dtgv)
+        {
+            int intAllRow = 0;
+
+            SqlConnection Conn = new SqlConnection(conn);
+            Conn.Open();
+            SqlCommand Comm = new SqlCommand(sql, Conn);
+            SqlDataAdapter da = new SqlDataAdapter(Comm);
+            DataSet ds = new DataSet();
+            da.Fill(ds, "Carton_Temp");
+            intAllRow = ds.Tables["Carton_Temp"].Rows.Count;
+            dtgv.DataSource = ds;
+            dtgv.DataMember = "Carton_Temp";
+            dtgv.Refresh();
+            da.Dispose();
+            ds.Dispose();
+            Comm.Dispose();
+            Conn.Close();
+            return intAllRow;   //เรียกจากที่นี้ต้องมี Return หากไม่มีการเปลี่ยนแปลงค่าก็แค่ return imt 0 ออกไปตาม default value
+        }
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string strConn = Properties.Settings.Default.strConn;
+            string strSql = "SELECT * FROM Carton";
+
+            showData(strSql, strConn, dgvFound);
         }
     }
 }
